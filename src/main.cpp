afd3c5baa23d4e0d33f6a6c8f9b02260d63043d3
shiba <shiba@shiba.com>
2019-10-14 14:14:26 +0900

    カメラ解像度やフォーカスを変えるライブラリを使ってビデオをキャプチャするようにした。

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,8 @@
 #include <opencv2/highgui/highgui.hpp>
 //キャリブレーションモジュール
 #include <opencv2/calib3d/calib3d.hpp>
-#include "webcam.h"
 
-
+#include "ewclib.h"
 
 
 #include <stdio.h>
@@ -21,45 +20,30 @@
 using namespace std;
 using namespace cv;
 
-int main() {
-    Mat image1;
+int main(char argc, char *argv[]) {
+    cv::Mat_<cv::Vec3b> image1;
 
-
-    VideoCapture cap1(0);
-    if (!cap1.isOpened()) { return -1; }
-    cap1.set(CV_CAP_PROP_FRAME_WIDTH, 1280);
-    cap1.set(CV_CAP_PROP_FRAME_HEIGHT, 720);
-
-    if (c_init()) { cout << "Error : c_init():" << endl; return 1; }
-
-    CHandle handle = c_open_device ("video0");
-    if (!handle) { cout << "Error : c_open_device():\n"; c_cleanup (); return 2; }
-
-    CControlValue value;
-    if (c_get_control(handle, CControlId(CC_FOCUS_ABSOLUTE), &value)) { std::cout << "Error : c_get_control(): " << endl; }
-    cout << value.value << endl;
-
-    value.value = 0;
-    if (c_set_control(handle, CControlId(CC_AUTO_FOCUS), &value)) { std::cout << "Error : c_set_control(): " << endl; }
-    if (c_set_control(handle, CControlId(CC_FOCUS_ABSOLUTE), &value)) { std::cout << "Error : c_set_control(): " << endl; }
-
+    int camera_number = 0;
+    if (argc == 2) {
+        camera_number = stoi(argv[1]);
+    }
+    int is_error = EWC_Open(camera_number, 1280, 720, 30, camera_number, MEDIASUBTYPE_RGB24);
+    EWC_SetValue(camera_number, EWC_FOCUS, 0.0);
+    image1 = cv::Mat_<cv::Vec3b>(720, 1280);
 
     char file_name[256];
     int itr = 0;
     while (1) {
-        cap1 >> image1;
+        EWC_GetImage(camera_number, image1.data);
 
         imshow("image1", image1);
         if (waitKey(33) == 32) { // 32 = space key
             printf("%d.jpg\n", itr);
-            sprintf(file_name, "%d.jpg", itr++);
+            sprintf_s(file_name, "%d.jpg", itr++);
             imwrite(file_name, image1);
         }
 
         if (waitKey(1) == 27) { break; }
     }
-    cap1.release();
-    c_close_device (handle);
-    c_cleanup ();
     return 0;
 }
